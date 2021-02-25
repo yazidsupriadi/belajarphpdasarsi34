1897e9b3a0a84cad33acd35be19428c5c11eb6ef
yazidsupriadi <yazidsupriadi14@gmail.com>
2021-02-25 15:24:55 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nama = 'yazid';
+    $angka = 12;
+    $angka_desimal = 12.7;
+
+    echo "nama saya adalah :".$nama."kelas berapa :".$angka;
+    echo "<br>";
+    echo "sekarang adik saya sekolah di kelas ".$angka;
+    echo "<br>";
+
+    echo 'hello world! selemat hari "jumat"';
+
+    echo "<h1>hello world</h1>";
+
+
+    echo "dokumen :".$_SERVER["DOCUMENT_ROOT"];
+    
+    echo "dokumen :".$_SERVER["PHP_SELF"];
+?>
---
+++ lingkaran.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('PHI',3.14);
+    
+    $jari_jari = 7;
+    $jari_jari = 21;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo "luas lingkaran adalah :".$luas_lingkaran;
+?>
